05d19da349aae19265b4c15caaac82d35dc660a1
Jimmy <101053639+JimmyJimbolo@users.noreply.github.com>
2022-10-06 12:36:47 +0200

    Update WiFi.ino

--- HA_Cocina_v2.2/WiFi.ino
+++ HA_Cocina_v2.2/WiFi.ino
@@ -1,6 +1,6 @@
 void Wificonnect() {
   WiFi.mode(WIFI_STA);
-  WiFi.begin("Jimbopolis", "aquiles13aquiles13");
+  WiFi.begin("USER", "PASS");  //  Usuario y contraseña de la red WiFi
   while (WiFi.status() != WL_CONNECTED) { delay(200); }
   ip = WiFi.localIP().toString();
 }
